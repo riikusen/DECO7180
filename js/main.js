faff72693d92aca922ecc7a642988ab69eacba59
Jade Li <648154091@qq.com>
2021-10-21 18:38:30 +1000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+// Colour
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+// Habitat
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function showSlides2(n) {
+  var i2;
+  var slides2 = document.getElementsByClassName("mySlides2");
+  if (n > slides2.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i2 = 0; i2 < slides2.length; i2++) {
+    slides2[i2].style.display = "none";
+  }
+  slides2[slideIndex2-1].style.display = "block";
+}
+
+var slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+// Next/previous controls
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}
+
+function showSlides3(n) {
+  var i3;
+  var slides3 = document.getElementsByClassName("mySlides3");
+  if (n > slides3.length) {slideIndex3 = 1}
+  if (n < 1) {slideIndex3 = slides3.length}
+  for (i3 = 0; i3 < slides3.length; i3++) {
+    slides3[i3].style.display = "none";
+  }
+  slides3[slideIndex3-1].style.display = "block";
+}
+
